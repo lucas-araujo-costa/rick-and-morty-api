a4585d837b573c19d3592c72534c5eb7a5d0f99d
Lucas <lucasaraujo1964@gmail.com>
2024-03-26 17:22:18 -0300

    feat: adding errorpage element: when accessing an invalid route, returns an error page

---
+++ src/Pages/ErrorPage.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header/Header";
+
+export default function ErrorPage() {
+  return (
+    <>
+      <Header />
+      <div className="text-white text-center flex justify-center items-center mt-96">Error: page not found.</div>
+    </>
+  )
+}
