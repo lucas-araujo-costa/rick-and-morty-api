83994900a1eb272d7bb8bb762d5ed1dc52bc47c9
Lucas <lucasaraujo1964@gmail.com>
2024-03-26 17:19:11 -0300

    feat: project configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-default": "#0E0A1B",
+      },
+      backgroundImage: {
+        "hero": "url('https://i.imgur.com/ADqwK2l.png')"
+      }
+    },
   },
   plugins: [],
 }
